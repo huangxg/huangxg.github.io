65f06e938d733c114cd8c55687144ced6c8da1fb
huangxg <huang.xingang@gmail.com>
2019-09-08 16:59:39 -0700

    add 三星 LG 春兰 围棋赛

--- go/go.js
+++ go/go.js
@@ -1,7 +1,7 @@
 var bao = bao || {};
 
-bao.go = (() => {
-  var news = [
+bao.go = {
+  news : [
     {
       title : '围棋资源',
       date  : '2019-09-01',
@@ -10,10 +10,31 @@ bao.go = (() => {
       title : '1988--2015 国际围棋大赛排行榜',
       date  : '2015-10-04',
       url   : 'go/ranking.html'
+    }, {
+      title : '1996--2019 历届三星杯八强',
+      date  : '2019-09-08',
+      url   : 'go/top8_samsung.html'
+    }, {
+      title : '1997--2019 历届 LG 杯八强',
+      date  : '2019-09-08',
+      url   : 'go/top8_lg.html'
+    }, {
+      title : '1999--2019 历届春兰杯八强',
+      date  : '2019-09-08',
+      url   : 'go/top8_chunlan.html'
     }
-  ];
+  ],
 
-  return {
-    news : news
-  };
-})();
+  top8TableHeaders : [
+    { title: '届数', align: 'center' },
+    { title: '年份' },
+    { title: '冠军' },
+    { title: '亚军' },
+    { title: '第三名' },
+    { title: '第四名' },
+    { title: '八强' },
+    { title: '八强' },
+    { title: '八强' },
+    { title: '八强' }
+  ]
+};
