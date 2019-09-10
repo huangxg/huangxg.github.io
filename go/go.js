fe69f8c2da3e539669f6bfc02332cbae98ab242e
huangxg <huang.xingang@gmail.com>
2019-09-09 21:15:38 -0700

    more Go data

--- go/go.js
+++ go/go.js
@@ -7,9 +7,21 @@ bao.go = {
       date  : '2019-09-01',
       url   : 'go/res.html'
     }, {
-      title : '1988--2015 国际围棋大赛排行榜',
-      date  : '2015-10-04',
+      title : '1988--2019 国际围棋大赛排行榜',
+      date  : '2019-09-09',
       url   : 'go/ranking.html'
+    }, {
+      title : '1988--2011 历届富士通杯八强（停办）',
+      date  : '2011-10-30',
+      url   : 'go/top8_fujitsu.html'
+    }, {
+      title : '1989--2013 历届应氏杯八强',
+      date  : '2013-03-06',
+      url   : 'go/top8_ing.html'
+    }, {
+      title : '1992--1998 历届东洋证券杯八强（停办）',
+      date  : '2008-03-23',
+      url   : 'go/top8_tongyang.html'
     }, {
       title : '1996--2019 历届三星杯八强',
       date  : '2019-09-08',
@@ -22,6 +34,26 @@ bao.go = {
       title : '1999--2019 历届春兰杯八强',
       date  : '2019-09-08',
       url   : 'go/top8_chunlan.html'
+    }, {
+      title : '2003--2009 历届丰田杯八强（停办）',
+      date  : '2009-01-11',
+      url   : 'go/top8_toyota.html'
+    }, {
+      title : '2009--2012 历届 BC 卡杯八强（停办）',
+      date  : '2009-05-16',
+      url   : 'go/top8_bc.html'
+    }, {
+      title : '2013--2019 历届百灵杯八强',
+      date  : '2019-09-09',
+      url   : 'go/top8_bailing.html'
+    }, {
+      title : '2013--2018 历届梦百合杯八强',
+      date  : '2019-09-09',
+      url   : 'go/top8_mlily.html'
+    }, {
+      title : '2016 首届新奥杯八强',
+      date  : '2019-09-09',
+      url   : 'go/top8_xinao.html'
     }
   ],
 
--- go/ranking.js
+++ go/ranking.js
@@ -2,6 +2,63 @@ var bao = bao || {};
 
 bao.go = bao.go || {};
 
+bao.go.ranking = (() => {
+  function getPlayers(matchDataArray) {
+    var players = [];
+
+    matchDataArray.forEach(match => {
+      match.forEach(row => {
+        Array.prototype.push.apply(players, row.slice(2));
+      });
+    });
+
+    return [... new Set(players)];
+  }
+
+  function getResults(matchDataArray) {
+    var players = getPlayers(matchDataArray),
+        results = {};
+
+    players.forEach(player => {
+      results[player] = [0, 0, 0, 0];
+    });
+
+    matchDataArray.forEach(match => {
+      match.forEach(row => {
+        row.slice(2).forEach((player, i) => {
+          if (i == 3) {
+            i = 2;
+          } else if (i > 3) {
+            i = 3;
+          }
+          results[player][i]++;
+        });
+      });
+    });
+
+    var array = [];
+    Object.keys(results).forEach(player =>{
+      array.push([player, ...results[player]]);
+    });
+    
+    return array;
+  }
+
+  function calcScores(results) {
+    results.forEach( row => {
+      var score = 8.07 * row[1] + 4.03 * row[2] + 2.01 * row[3] + row[4];
+      row.push(score);
+    });
+    return results;
+  }
+
+  return {
+    calcScores : calcScores,
+    getPlayers : getPlayers,
+    getResults : getResults,
+  };
+})();
+
 bao.go.playerRanking = (() => {
   var cols = [
     { title: '棋手', type: 'string', align: 'center'}, 
@@ -10,8 +67,9 @@ bao.go.playerRanking = (() => {
     { title: '四强', type: 'number', decimal: 0, align: 'right'}, 
     { title: '八强', type: 'number', decimal: 0, align: 'right'}, 
     { title: '积分', type: 'number', decimal: 2, align: 'right'}
-  ],
-      data = [
+  ];
+
+  var data = [
     ['李昌镐', 17, 11, 13, 11],
     ['李世石', 14, 5, 5, 12],
     ['曹薰铉', 9, 2, 9, 12],
@@ -143,32 +201,35 @@ bao.go.playerRanking = (() => {
     ['陈时映', 0, 0, 0, 1]
   ];
 
-  data.forEach( row => {
-    var score = 8.07 * row[1] + 4.03 * row[2] + 2.01 * row[3] + row[4];
-    row.push(score);
-  });
-
   return {
     cols : cols,
-    data : data
+    data : (() => {
+      data.forEach( row => {
+        var score = 8.07 * row[1] + 4.03 * row[2] + 2.01 * row[3] + row[4];
+        row.push(score);
+      });
+      return data;
+    })(),
   };
 })();
 
 bao.go.countryRanking = (() => {
   var cols = [
-    { title: '国家', type: 'string', align: 'right'}, 
+    { title: '国家', type: 'string', align: 'center'}, 
     { title: '冠军', type: 'number', decimal: 0, align: 'right'},
     { title: '亚军', type: 'number', decimal: 0, align: 'right'},
     { title: '四强', type: 'number', decimal: 0, align: 'right'},
     { title: '八强', type: 'number', decimal: 0, align: 'right'},
     { title: '棋手', type: 'number', decimal: 0, align: 'right'},
     { title: '积分', type: 'number', decimal: 2, align: 'right'}
-  ],
-      data = [
-    ['中', 29, 41, 76, 162, 57],
-    ['日', 11, 18, 37, 97, 29],
-    ['韩', 57, 38, 81, 125, 41],
-    ['其他', 0, 0, 0, 4, 2]
+  ];
+
+  var data = [
+    ['中', 43, 48, 97, 199, 73],
+    ['日', 10, 17, 33, 96, 28],
+    ['韩', 61, 49, 98, 157, 48],
+    ['其他', 0, 0, 0, 4, 2],
+    ['合计', 114, 114, 228, 456, 151]
   ];
 
   data.forEach( row => {
