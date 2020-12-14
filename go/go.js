459c105e41dab2195149130351d770f83ef20bba
huangxg <huang.xingang@gmail.com>
2020-12-13 21:09:17 -0800

    update for LG, 新奥, 天府

--- go/go.js
+++ go/go.js
@@ -4,31 +4,31 @@ bao.go = {
   news : [
     {
       title : '围棋资源',
-      date  : '2019-09-01',
+      date  : '2020-09-09',
       url   : 'go/res.html'
     }, {
-      title : '1988--2019 国际围棋大赛排行榜',
-      date  : '2019-09-09',
+      title : '1988--2020 国际围棋大赛排行榜',
+      date  : '2020-12-13',
       url   : 'go/ranking.html'
     }, {
       title : '1988--2011 历届富士通杯八强（停办）',
       date  : '2011-10-30',
       url   : 'go/top8_fujitsu.html'
     }, {
-      title : '1989--2013 历届应氏杯八强',
-      date  : '2013-03-06',
+      title : '1989--2016 历届应氏杯八强',
+      date  : '2016-10-27',
       url   : 'go/top8_ing.html'
     }, {
       title : '1992--1998 历届东洋证券杯八强（停办）',
       date  : '2008-03-23',
       url   : 'go/top8_tongyang.html'
     }, {
-      title : '1996--2019 历届三星杯八强',
-      date  : '2019-09-08',
+      title : '1996--2020 历届三星杯八强',
+      date  : '2020-11-04',
       url   : 'go/top8_samsung.html'
     }, {
-      title : '1997--2019 历届 LG 杯八强',
-      date  : '2019-09-08',
+      title : '1997--2020 历届 LG 杯八强',
+      date  : '2020-02-14',
       url   : 'go/top8_lg.html'
     }, {
       title : '1999--2019 历届春兰杯八强',
@@ -51,8 +51,12 @@ bao.go = {
       date  : '2019-09-09',
       url   : 'go/top8_mlily.html'
     }, {
-      title : '2016 首届新奥杯八强',
+      title : '2016 首届新奥杯八强（停办）',
       date  : '2019-09-09',
+      url   : 'go/top8_xinao.html'
+    }, {
+      title : '2018 首届天府杯八强',
+      date  : '2018-12-27',
       url   : 'go/top8_xinao.html'
     }
   ],
---
+++ go/top8_tianfu.js
@@ -0,0 +1,10 @@
+var bao = bao || {};
+
+bao.go = bao.go || {};
+
+bao.go.tianfuTop8 = {
+  cols : bao.go.top8TableHeaders,
+  data : [
+    [1, 2018, '陈耀烨', '申真谞', '朴廷桓', '江维杰', '辜梓豪', '唐韦星', '檀啸', '连笑'],
+  ]
+};
